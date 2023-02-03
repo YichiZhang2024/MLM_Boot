acf38e523a26abf45e477b2d5f734a054c032b7f
Winnie Wing-Yee Tse <wingyeet@usc.edu>
2023-02-02 17:13:21 -0800

    Computed CIs for random effects

--- draft/illustration_boot.R
+++ draft/illustration_boot.R
@@ -50,6 +50,12 @@ icc <- function(mod) {
   c(icc_est = icc_est, icc_var = icc_var)
 }
 
+# Function for obtaining random effects
+raneff <- function(mod) {
+  raneff <- as.data.frame(VarCorr(mod))$vcov
+  c(raneff = raneff)
+}
+
 # Parametric bootstrap ---------------------------------------------------------
 
 # Fixed effects and Rsq
@@ -59,6 +65,10 @@ saveRDS(boo_par_fr, "draft/illustration_boo_res/boo_par_fr.rds")
 # ICC
 boo_par_icc <- bootstrap_mer(mod_0, icc, nsim = 1999L, type = "parametric")
 saveRDS(boo_par_icc, "draft/illustration_boo_res/boo_par_icc.rds")
+
+# Random effects
+boo_par_ran <- bootstrap_mer(mod_f, raneff, nsim = 1999L, type = "parametric")
+saveRDS(boo_par_ran, "draft/illustration_boo_res/boo_par_ran.rds")
 
 # Residual bootstrap -----------------------------------------------------------
 
@@ -73,6 +83,11 @@ boo_cgr_icc <- bootstrap_mer(mod_0, icc, nsim = 1999L, type = "residual_cgr")
 saveRDS(boo_res_icc, "draft/illustration_boo_res/boo_res_icc.rds")
 saveRDS(boo_cgr_icc, "draft/illustration_boo_res/boo_cgr_icc.rds")
 
+boo_res_ran <- bootstrap_mer(mod_f, raneff, nsim = 1999L, type = "residual")
+boo_cgr_ran <- bootstrap_mer(mod_f, raneff, nsim = 1999L, type = "residual_cgr")
+saveRDS(boo_res_ran, "draft/illustration_boo_res/boo_res_ran.rds")
+saveRDS(boo_cgr_ran, "draft/illustration_boo_res/boo_cgr_ran.rds")
+
 # Influence values
 inf_val_fr <- lapply(1:9, function(i) {
   empinf_mer(mod_f, fixef_rsq, index = i)
@@ -81,6 +96,11 @@ saveRDS(inf_val_fr, "draft/illustration_boo_res/inf_val_fr.rds")
 
 inf_val_icc <- empinf_mer(mod_0, icc, index = 1)
 saveRDS(inf_val_icc, "draft/illustration_boo_res/inf_val_icc.rds")
+
+inf_val_ran <- lapply(1:4, function(i) {
+  empinf_mer(mod_f, raneff, index = i)
+})
+saveRDS(inf_val_ran, "draft/illustration_boo_res/inf_val_ran.rds")
 
 # Case bootstrap ---------------------------------------------------------------
 
@@ -96,6 +116,12 @@ boo_cas1_icc <- bootstrap_mer(mod_0, icc, nsim = 1999L, type = "case",
 saveRDS(boo_cas_icc, "draft/illustration_boo_res/boo_cas_icc.rds")
 saveRDS(boo_cas1_icc, "draft/illustration_boo_res/boo_cas1_icc.rds")
 
+boo_cas_ran <- bootstrap_mer(mod_f, raneff, nsim = 1999L, type = "case")
+boo_cas1_ran <- bootstrap_mer(mod_f, raneff, nsim = 1999L, type = "case", 
+                             lv1_resample = TRUE)
+saveRDS(boo_cas_ran, "draft/illustration_boo_res/boo_cas_ran.rds")
+saveRDS(boo_cas1_ran, "draft/illustration_boo_res/boo_cas1_ran.rds")
+
 # with lmeresampler
 boo_cas1_lr_icc <- case_bootstrap(mod_0, .f = icc, 
                                   B = 1999L, resample = c(TRUE, TRUE))
@@ -109,27 +135,37 @@ saveRDS(boo_wil_fr, "draft/illustration_boo_res/boo_wil_fr.rds")
 boo_wil_icc <- wild_bootstrap(mod_0, .f = icc, B = 1999L)
 saveRDS(boo_wil_icc, "draft/illustration_boo_res/boo_wil_icc.rds")
 
+boo_wil_ran <- wild_bootstrap(mod_f, .f = raneff, B = 1999L)
+saveRDS(boo_wil_ran, "draft/illustration_boo_res/boo_wil_ran.rds")
+
 
 # Load boot results ------------------------------------------------------------
 
 boo_par_fr <- readRDS("draft/illustration_boo_res/boo_par_fr.rds")
 boo_par_icc <- readRDS("draft/illustration_boo_res/boo_par_icc.rds")
+boo_par_ran <- readRDS("draft/illustration_boo_res/boo_par_ran.rds")
 
 boo_res_fr <- readRDS("draft/illustration_boo_res/boo_res_fr.rds")
 boo_cgr_fr <- readRDS("draft/illustration_boo_res/boo_cgr_fr.rds")
 boo_res_icc <- readRDS("draft/illustration_boo_res/boo_res_icc.rds")
 boo_cgr_icc <- readRDS("draft/illustration_boo_res/boo_cgr_icc.rds")
+boo_res_ran <- readRDS("draft/illustration_boo_res/boo_res_ran.rds")
+boo_cgr_ran <- readRDS("draft/illustration_boo_res/boo_cgr_ran.rds")
 
 inf_val_fr <- readRDS("draft/illustration_boo_res/inf_val_fr.rds")
 inf_val_icc <- readRDS("draft/illustration_boo_res/inf_val_icc.rds")
+inf_val_ran <- readRDS("draft/illustration_boo_res/inf_val_ran.rds")
 
 boo_cas_fr <- readRDS("draft/illustration_boo_res/boo_cas_fr.rds")
 boo_cas1_fr <- readRDS("draft/illustration_boo_res/boo_cas1_fr.rds")
 boo_cas_icc <- readRDS("draft/illustration_boo_res/boo_cas_icc.rds")
 boo_cas1_icc <- readRDS("draft/illustration_boo_res/boo_cas1_icc.rds")
+boo_cas_ran <- readRDS("draft/illustration_boo_res/boo_cas_ran.rds")
+boo_cas1_ran <- readRDS("draft/illustration_boo_res/boo_cas1_ran.rds")
 
 boo_wil_fr <- readRDS("draft/illustration_boo_res/boo_wil_fr.rds")
 boo_wil_icc <- readRDS("draft/illustration_boo_res/boo_wil_icc.rds")
+boo_wil_ran <- readRDS("draft/illustration_boo_res/boo_wil_ran.rds")
 
 
 # Confidence intervals ---------------------------------------------------------
@@ -139,6 +175,9 @@ boo_par_fr_ci <- lapply(1:9, function(i) {
   boot.ci(boo_par_fr, type = c("norm", "basic", "perc"), index = i)
 })
 boo_par_icc_ci <- boot.ci(boo_par_icc, type = c("norm", "basic", "perc"))
+boo_par_ran_ci <- lapply(1:4, function(i) {
+  boot.ci(boo_par_ran, type = c("norm", "basic", "perc"), index = i)
+})
 
 # Residual bootstrap
 boo_res_fr_ci <- lapply(1:9, function(i) {
@@ -148,6 +187,10 @@ boo_res_fr_ci <- lapply(1:9, function(i) {
 boo_res_icc_ci <- boot::boot.ci(boo_res_icc, 
                                 type = c("norm", "basic", "perc", "bca", "stud"), 
                                 L = inf_val_icc)
+boo_res_ran_ci <- lapply(1:4, function(i) {
+  boot::boot.ci(boo_res_ran, type = c("norm", "basic", "perc", "bca"), 
+                index = i, L = inf_val_ran[[i]])
+})
 
 # Cases bootstrap (resampling only clusters)
 boo_cas_fr_ci <- lapply(1:9, function(i) {
@@ -157,19 +200,28 @@ boo_cas_fr_ci <- lapply(1:9, function(i) {
 boo_cas_icc_ci <- boot::boot.ci(boo_cas_icc, 
                                 type = c("norm", "basic", "perc", "bca", "stud"), 
                                 L = inf_val_icc)
+boo_cas_ran_ci <- lapply(1:4, function(i) {
+  boot::boot.ci(boo_cas_ran, type = c("norm", "basic", "perc", "bca"), 
+                index = i, L = inf_val_ran[[i]])
+})
 
 # Cases bootstrap (resampling both clusters and individuals)
 boo_cas1_fr_ci <- lapply(1:9, function(i) {
-  boot::boot.ci(boo_cas1_fr, type = c("norm", "basic", "perc", "bca"), 
+  boot::boot.ci(boo_cas1_fr, type = c("norm", "basic", "perc", "bca"),
                 index = i, L = inf_val_fr[[i]])
 })
-boo_cas1_icc_ci <- boot::boot.ci(boo_cas1_icc, 
-                                 type = c("norm", "basic", "perc", "bca", "stud"), 
+boo_cas1_icc_ci <- boot::boot.ci(boo_cas1_icc,
+                                 type = c("norm", "basic", "perc", "bca", "stud"),
                                  L = inf_val_icc)
+boo_cas1_ran_ci <- lapply(1:4, function(i) {
+  boot::boot.ci(boo_cas1_ran, type = c("norm", "basic", "perc", "bca"),
+                index = i, L = inf_val_ran[[i]])
+})
 
 # Wild bootstrap
 boo_wil_fr_ci <- confint(boo_wil_fr, method = c("norm", "basic", "perc", "bca"))
 boo_wil_icc_ci <- confint(boo_wil_icc, method = c("norm", "basic", "perc", "bca"))
+boo_wil_ran_ci <- confint(boo_wil_ran, method = c("norm", "basic", "perc", "bca"))
 
 # Logit transformation
 boo_par_rsq_ci <- boot.ci(boo_par_fr, h = qlogis, 
@@ -239,80 +291,106 @@ get_ci <- function(boo_ci, type) {
   }
 }
 
-orig_est <- c(boo_par_fr$t0, NA, boo_par_icc$t0[1]) |> round(2)
+orig_est <- c(boo_par_fr$t0, NA, boo_par_icc$t0[1], boo_par_ran$t0) |> round(2)
 boo_est <- t(data.frame(
-  par = c(get_est(boo_par_fr), NA, get_est(boo_par_icc)[1]), 
-  res = c(get_est(boo_res_fr), NA, get_est(boo_res_icc)[1]), 
-  wild = c(get_est(boo_wil_fr), NA, get_est(boo_wil_icc)[1]), 
-  cas = c(get_est(boo_cas_fr), NA, get_est(boo_cas_icc)[1]), 
-  cas1 = c(get_est(boo_cas1_fr), NA, get_est(boo_cas1_icc)[1])
+  par = c(get_est(boo_par_fr), NA, get_est(boo_par_icc)[1], 
+          get_est(boo_par_ran)), 
+  res = c(get_est(boo_res_fr), NA, get_est(boo_res_icc)[1], 
+          get_est(boo_res_ran)), 
+  wild = c(get_est(boo_wil_fr), NA, get_est(boo_wil_icc)[1], 
+           get_est(boo_wil_ran)), 
+  cas = c(get_est(boo_cas_fr), NA, get_est(boo_cas_icc)[1], 
+          get_est(boo_cas_ran)), 
+  cas1 = c(get_est(boo_cas1_fr), NA, get_est(boo_cas1_icc)[1], 
+           get_est(boo_cas1_ran))
 )) |> round(2)
 boo_se <- t(data.frame(
-  par = c(get_se(boo_par_fr), NA, get_se(boo_par_icc)[1]), 
-  res = c(get_se(boo_res_fr), NA, get_se(boo_res_icc)[1]), 
-  wild = c(get_se(boo_wil_fr), NA, get_se(boo_wil_icc)[1]), 
-  cas = c(get_se(boo_cas_fr), NA, get_se(boo_cas_icc)[1]), 
-  cas1 = c(get_se(boo_cas1_fr), NA, get_se(boo_cas1_icc)[1])
+  par = c(get_se(boo_par_fr), NA, get_se(boo_par_icc)[1], 
+          get_se(boo_par_ran)), 
+  res = c(get_se(boo_res_fr), NA, get_se(boo_res_icc)[1], 
+          get_se(boo_res_ran)), 
+  wild = c(get_se(boo_wil_fr), NA, get_se(boo_wil_icc)[1], 
+           get_se(boo_wil_ran)), 
+  cas = c(get_se(boo_cas_fr), NA, get_se(boo_cas_icc)[1], 
+          get_se(boo_cas_ran)), 
+  cas1 = c(get_se(boo_cas1_fr), NA, get_se(boo_cas1_icc)[1], 
+           get_se(boo_cas1_ran))
 )) |> round(2)
 norm_ci <- t(data.frame(
   par = c(get_ci(boo_par_fr_ci, "normal"), get_ci(boo_par_rsq_ci, "normal"), 
-          get_ci(boo_par_icc_ci, "normal")), 
+          get_ci(boo_par_icc_ci, "normal"), get_ci(boo_par_ran_ci, "normal")), 
   res = c(get_ci(boo_res_fr_ci, "normal"), get_ci(boo_res_rsq_ci, "normal"), 
-          get_ci(boo_res_icc_ci, "normal")), 
+          get_ci(boo_res_icc_ci, "normal"), get_ci(boo_res_ran_ci, "normal")), 
   wild = c(get_ci(boo_wil_fr_ci, "normal"), "()", 
-           get_ci(boo_wil_icc_ci, "normal")[1]), 
+           get_ci(boo_wil_icc_ci, "normal")[1], 
+           get_ci(boo_wil_ran_ci, "normal")), 
   cas = c(get_ci(boo_cas_fr_ci, "normal"), get_ci(boo_cas_rsq_ci, "normal"), 
-          get_ci(boo_cas_icc_ci, "normal")), 
+          get_ci(boo_cas_icc_ci, "normal"), get_ci(boo_cas_ran_ci, "normal")), 
   cas1 = c(get_ci(boo_cas1_fr_ci, "normal"), get_ci(boo_cas1_rsq_ci, "normal"), 
-           get_ci(boo_cas1_icc_ci, "normal"))
+           get_ci(boo_cas1_icc_ci, "normal"), rep(0, 4) # placeholders
+           # get_ci(boo_cas1_ran_ci, "normal")
+           )
 ))
 basic_ci <- t(data.frame(
   par = c(get_ci(boo_par_fr_ci, "basic"), get_ci(boo_par_rsq_ci, "basic"), 
-          get_ci(boo_par_icc_ci, "basic")), 
+          get_ci(boo_par_icc_ci, "basic"), get_ci(boo_par_ran_ci, "basic")), 
   res = c(get_ci(boo_res_fr_ci, "basic"), get_ci(boo_res_rsq_ci, "basic"), 
-          get_ci(boo_res_icc_ci, "basic")), 
+          get_ci(boo_res_icc_ci, "basic"), get_ci(boo_res_ran_ci, "basic")), 
   wild = c(get_ci(boo_wil_fr_ci, "basic"), "()", 
-           get_ci(boo_wil_icc_ci, "basic")[1]), 
+           get_ci(boo_wil_icc_ci, "basic")[1], 
+           get_ci(boo_wil_ran_ci, "basic")), 
   cas = c(get_ci(boo_cas_fr_ci, "basic"), get_ci(boo_cas_rsq_ci, "basic"), 
-          get_ci(boo_cas_icc_ci, "basic")), 
+          get_ci(boo_cas_icc_ci, "basic"), get_ci(boo_cas_ran_ci, "basic")), 
   cas1 = c(get_ci(boo_cas1_fr_ci, "basic"), get_ci(boo_cas1_rsq_ci, "basic"), 
-           get_ci(boo_cas1_icc_ci, "basic"))
+           get_ci(boo_cas1_icc_ci, "basic"), rep(0, 4) # placeholders
+           # get_ci(boo_cas1_ran_ci, "basic")
+           )
 ))
 perc_ci <- t(data.frame(
   par = c(get_ci(boo_par_fr_ci, "percent"), get_ci(boo_par_rsq_ci, "percent"), 
-          get_ci(boo_par_icc_ci, "percent")), 
+          get_ci(boo_par_icc_ci, "percent"), get_ci(boo_par_ran_ci, "percent")), 
   res = c(get_ci(boo_res_fr_ci, "percent"), get_ci(boo_res_rsq_ci, "percent"), 
-          get_ci(boo_res_icc_ci, "percent")),
+          get_ci(boo_res_icc_ci, "percent"), get_ci(boo_res_ran_ci, "percent")),
   wild = c(get_ci(boo_wil_fr_ci, "percent"), "()", 
-           get_ci(boo_wil_icc_ci, "percent")[1]), 
+           get_ci(boo_wil_icc_ci, "percent")[1], 
+           get_ci(boo_wil_ran_ci, "percent")), 
   cas = c(get_ci(boo_cas_fr_ci, "percent"), get_ci(boo_cas_rsq_ci, "percent"), 
-          get_ci(boo_cas_icc_ci, "percent")), 
+          get_ci(boo_cas_icc_ci, "percent"), get_ci(boo_cas_ran_ci, "percent")), 
   cas1 = c(get_ci(boo_cas1_fr_ci, "percent"), get_ci(boo_cas1_rsq_ci, "percent"), 
-           get_ci(boo_cas1_icc_ci, "percent"))
+           get_ci(boo_cas1_icc_ci, "percent"), rep(0, 4) # placeholders
+           # get_ci(boo_cas1_ran_ci, "percent")
+           )
 ))
 stud_ci <- t(data.frame(
-  par = c(rep("()", each = 10), get_ci(boo_par_icc_ci, "student")), 
-  res = c(rep("()", each = 10), get_ci(boo_res_icc_ci, "student")), 
+  par = c(rep("()", each = 10), get_ci(boo_par_icc_ci, "student"), 
+          rep("()", each = 4)), 
+  res = c(rep("()", each = 10), get_ci(boo_res_icc_ci, "student"), 
+          rep("()", each = 4)), 
   wild = "()", 
-  cas = c(rep("()", each = 10), get_ci(boo_cas_icc_ci, "student")), 
-  cas1 = c(rep("()", each = 10), get_ci(boo_cas1_icc_ci, "student"))
+  cas = c(rep("()", each = 10), get_ci(boo_cas_icc_ci, "student"), 
+          rep("()", each = 4)), 
+  cas1 = c(rep("()", each = 10), get_ci(boo_cas1_icc_ci, "student"), 
+           rep("()", each = 4))
 ))
 bca_ci <- t(data.frame(
   par = c(get_ci(boo_par_fr_ci, "bca"), get_ci(boo_par_rsq_ci, "bca"), 
-          get_ci(boo_par_icc_ci, "bca")), 
+          get_ci(boo_par_icc_ci, "bca"), get_ci(boo_par_ran_ci, "bca")), 
   res = c(get_ci(boo_res_fr_ci, "bca"), get_ci(boo_res_rsq_ci, "bca"), 
-          get_ci(boo_res_icc_ci, "bca")), 
+          get_ci(boo_res_icc_ci, "bca"), get_ci(boo_res_ran_ci, "bca")), 
   wild = "()", 
   cas = c(get_ci(boo_cas_fr_ci, "bca"), get_ci(boo_cas_rsq_ci, "bca"), 
-          get_ci(boo_cas_icc_ci, "bca")), 
+          get_ci(boo_cas_icc_ci, "bca"), get_ci(boo_cas_ran_ci, "bca")), 
   cas1 = c(get_ci(boo_cas1_fr_ci, "bca"), get_ci(boo_cas1_rsq_ci, "bca"), 
-           get_ci(boo_cas1_icc_ci, "bca"))
+           get_ci(boo_cas1_icc_ci, "bca"), rep(0, 4) # placeholders 
+           # get_ci(boo_cas1_ran_ci, "bca")
+           )
 ))
 
 boo_tab <- rbind(
   orig_est, boo_est, boo_se, norm_ci, basic_ci, perc_ci, stud_ci, bca_ci
 )
-colnames(boo_tab) <- c(names(fixef(mod_f)), "rsq", "rsq_trans", "icc")
+colnames(boo_tab) <- c(names(fixef(mod_f)), "rsq", "rsq_trans", "icc", 
+                       "tau00sq", "tau11sq", "tau01", "sigmasq")
 rownames(boo_tab) <- NULL
 compare_boo <- data.frame(
   stat = c("Original Est.", 
